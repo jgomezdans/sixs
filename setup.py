59aefd972a426fc29d460b930f7e5ad4dadf5aaa
Jose Gomez-Dans <jgomezdans@gmail.com>
2012-05-18 16:33:07 +0100

    It builds...

--- setup.py
+++ setup.py
@@ -1,105 +1,128 @@
-from setuptools import setup, find_packages
+from numpy.distutils.core import setup
+
 import sys, os
 
 version = '1.0'
 
-def configuration(parent_package='',top_path=None):
+def configuration ( parent_package='', top_path=None ):
     from numpy.distutils.misc_util import Configuration
-    config = Configuration('eoldaslib',parent_package,top_path, )
+    config = Configuration('sixs',parent_package,top_path, )
     fflags= '-fdefault-real-8 -ffixed-form' 
     
     config.add_extension ('lib_sixs_mcrm', 
-    sources = [ "eoldaslib/sixs_obs/lib_sixs_mcrm.pyf", \
-        "eoldaslib/sixs_obs/AATSR.f", "eoldaslib/sixs_obs/ABSTRA.f", \
-        "eoldaslib/sixs_obs/AEROPROF.f", "eoldaslib/sixs_obs/AEROSO.f", \
-        "eoldaslib/sixs_obs/AKTOOL.f", "eoldaslib/sixs_obs/ALI.f", \
-        "eoldaslib/sixs_obs/ASTER.f", "eoldaslib/sixs_obs/ATMREF.f", \
-        "eoldaslib/sixs_obs/AVHRR.f", "eoldaslib/sixs_obs/BBM.f", \
-        "eoldaslib/sixs_obs/BDM.f", "eoldaslib/sixs_obs/BRDFGRID.f", \
-        "eoldaslib/sixs_obs/CHAND.f", "eoldaslib/sixs_obs/CLEARW.f", \
-        "eoldaslib/sixs_obs/CSALBR.f", "eoldaslib/sixs_obs/DICA1.f", \
-        "eoldaslib/sixs_obs/DICA2.f", "eoldaslib/sixs_obs/DICA3.f", \
-        "eoldaslib/sixs_obs/DISCOM.f", "eoldaslib/sixs_obs/DISCRE.f", \
-        "eoldaslib/sixs_obs/DUST.f", "eoldaslib/sixs_obs/ENVIRO.f", \
-        "eoldaslib/sixs_obs/EQUIVWL.f", "eoldaslib/sixs_obs/ETM.f", \
-        "eoldaslib/sixs_obs/GAUSS.f", "eoldaslib/sixs_obs/GLI.f", \
-        "eoldaslib/sixs_obs/GOES.f", "eoldaslib/sixs_obs/HAPKALBE.f", \
-        "eoldaslib/sixs_obs/HAPKBRDF.f", "eoldaslib/sixs_obs/HRV.f", \
-        "eoldaslib/sixs_obs/HYPBLUE.f", "eoldaslib/sixs_obs/IAPIALBE.f", \
-        "eoldaslib/sixs_obs/IAPIBRDF.f", "eoldaslib/sixs_obs/IAPITOOLS.f", \
-        "eoldaslib/sixs_obs/INTERP.f", "eoldaslib/sixs_obs/ISO.f", \
-        "eoldaslib/sixs_obs/KERNEL.f", "eoldaslib/sixs_obs/KERNELPOL.f", \
-        "eoldaslib/sixs_obs/LAKEW.f", \
-        "eoldaslib/sixs_obs/main_ssssss.f", "eoldaslib/sixs_obs/MAS.f", \
-        "eoldaslib/sixs_obs/MERIS.f", "eoldaslib/sixs_obs/METEO.f", \
-        "eoldaslib/sixs_obs/METH1.f", "eoldaslib/sixs_obs/METH2.f", \
-        "eoldaslib/sixs_obs/METH3.f", "eoldaslib/sixs_obs/METH4.f", \
-        "eoldaslib/sixs_obs/METH5.f", "eoldaslib/sixs_obs/METH6.f", \
-        "eoldaslib/sixs_obs/MIDSUM.f", "eoldaslib/sixs_obs/MIDWIN.f", \
-        "eoldaslib/sixs_obs/MIE.f", "eoldaslib/sixs_obs/MINNALBE.f", \
-        "eoldaslib/sixs_obs/MINNBRDF.f", "eoldaslib/sixs_obs/MOCA1.f", \
-        "eoldaslib/sixs_obs/MOCA2.f", "eoldaslib/sixs_obs/MOCA3.f", \
-        "eoldaslib/sixs_obs/MOCA4.f", "eoldaslib/sixs_obs/MOCA5.f", \
-        "eoldaslib/sixs_obs/MOCA6.f", "eoldaslib/sixs_obs/MODISALBE.f", \
-        "eoldaslib/sixs_obs/MODISBRDF.f", "eoldaslib/sixs_obs/MODIS.f", \
-        "eoldaslib/sixs_obs/MSS.f", "eoldaslib/sixs_obs/NIOX1.f", \
-        "eoldaslib/sixs_obs/NIOX2.f", "eoldaslib/sixs_obs/NIOX3.f", \
-        "eoldaslib/sixs_obs/NIOX4.f", "eoldaslib/sixs_obs/NIOX5.f", \
-        "eoldaslib/sixs_obs/NIOX6.f", "eoldaslib/sixs_obs/OCEAALBE.f", \
-        "eoldaslib/sixs_obs/OCEABRDF.f", "eoldaslib/sixs_obs/OCEABRDFFAST.f", \
-        "eoldaslib/sixs_obs/OCEA.f", "eoldaslib/sixs_obs/OCEATOOLS.f", \
-        "eoldaslib/sixs_obs/ODA550.f", "eoldaslib/sixs_obs/ODRAYL.f", \
-        "eoldaslib/sixs_obs/OS.f", "eoldaslib/sixs_obs/OSPOL.f", \
-        "eoldaslib/sixs_obs/OXYG3.f", "eoldaslib/sixs_obs/OXYG4.f", \
-        "eoldaslib/sixs_obs/OXYG5.f", "eoldaslib/sixs_obs/OXYG6.f", \
-        "eoldaslib/sixs_obs/OZON1.f", "eoldaslib/sixs_obs/PLANPOL.f", \
-        "eoldaslib/sixs_obs/POLDER.f", "eoldaslib/sixs_obs/POLGLIT.f", \
-        "eoldaslib/sixs_obs/POLNAD.f", "eoldaslib/sixs_obs/POSGE.f", \
-        "eoldaslib/sixs_obs/POSGW.f", "eoldaslib/sixs_obs/POSLAN.f", \
-        "eoldaslib/sixs_obs/POSMTO.f", "eoldaslib/sixs_obs/POSNOA.f", \
-        "eoldaslib/sixs_obs/POSSOL.f", "eoldaslib/sixs_obs/POSSPO.f", 
-        "eoldaslib/sixs_obs/PRESPLANE.f", "eoldaslib/sixs_obs/PRESSURE.f", \
-        "eoldaslib/sixs_obs/PRINT_ERROR.f", "eoldaslib/sixs_obs/RAHMALBE.f", \
-        "eoldaslib/sixs_obs/RAHMBRDF.f", "eoldaslib/sixs_obs/ROUJALBE.f", \
-        "eoldaslib/sixs_obs/ROUJBRDF.f", "eoldaslib/sixs_obs/SAND.f", \
-        "eoldaslib/sixs_obs/SCATRA.f", "eoldaslib/sixs_obs/SEAWIFS.f", \
-        "eoldaslib/sixs_obs/SOLIRR.f", "eoldaslib/sixs_obs/SOOT.f", \
-        "eoldaslib/sixs_obs/SPECINTERP.f", "eoldaslib/sixs_obs/SPLIE2.f", \
-        "eoldaslib/sixs_obs/SPLIN2.f", "eoldaslib/sixs_obs/SPLINE.f", \
-        "eoldaslib/sixs_obs/SPLINT.f", "eoldaslib/sixs_obs/STM.f", \
-        "eoldaslib/sixs_obs/SUBSUM.f", "eoldaslib/sixs_obs/SUBWIN.f", \
-        "eoldaslib/sixs_obs/TM.f", "eoldaslib/sixs_obs/TROPIC.f", \
-        "eoldaslib/sixs_obs/TRUNCA.f", "eoldaslib/sixs_obs/US62.f", \
-        "eoldaslib/sixs_obs/VARSOL.f", "eoldaslib/sixs_obs/VEGETA.f", \
-        "eoldaslib/sixs_obs/VERSALBE.f", "eoldaslib/sixs_obs/VERSBRDF.f", \
-        "eoldaslib/sixs_obs/VERSTOOLS.f", "eoldaslib/sixs_obs/VGT.f", \
-        "eoldaslib/sixs_obs/VIIRS.f", "eoldaslib/sixs_obs/WALTALBE.f", \
-        "eoldaslib/sixs_obs/WALTBRDF.f", "eoldaslib/sixs_obs/WATE.f", \
-        "eoldaslib/sixs_obs/WAVA1.f", "eoldaslib/sixs_obs/WAVA2.f", \
-        "eoldaslib/sixs_obs/WAVA3.f","eoldaslib/sixs_obs/WAVA4.f", \
-        "eoldaslib/sixs_obs/WAVA5.f","eoldaslib/sixs_obs/WAVA6.f", \
-        "eoldaslib/sixs_obs/sixs_mcrm_sim.f90"] )
+    sources = [ "sixs/lib_sixs_mcrm.pyf", \
+        "sixs/AATSR.f", "sixs/ABSTRA.f", \
+        "sixs/AEROPROF.f", "sixs/AEROSO.f", \
+        "sixs/AKTOOL.f", "sixs/ALI.f", \
+        "sixs/ASTER.f", "sixs/ATMREF.f", \
+        "sixs/AVHRR.f", "sixs/BBM.f", \
+        "sixs/BDM.f", "sixs/BRDFGRID.f", \
+        "sixs/CHAND.f", "sixs/CLEARW.f", \
+        "sixs/CSALBR.f", "sixs/DICA1.f", \
+        "sixs/DICA2.f", "sixs/DICA3.f", \
+        "sixs/DISCOM.f", "sixs/DISCRE.f", \
+        "sixs/DUST.f", "sixs/ENVIRO.f", \
+        "sixs/EQUIVWL.f", "sixs/ETM.f", \
+        "sixs/GAUSS.f", "sixs/GLI.f", \
+        "sixs/GOES.f", "sixs/HAPKALBE.f", \
+        "sixs/HAPKBRDF.f", "sixs/HRV.f", \
+        "sixs/HYPBLUE.f", "sixs/IAPIALBE.f", \
+        "sixs/IAPIBRDF.f", "sixs/IAPITOOLS.f", \
+        "sixs/INTERP.f", "sixs/ISO.f", \
+        "sixs/KERNEL.f", "sixs/KERNELPOL.f", \
+        "sixs/LAKEW.f", \
+        "sixs/main_ssssss.f", "sixs/MAS.f", \
+        "sixs/MERIS.f", "sixs/METEO.f", \
+        "sixs/METH1.f", "sixs/METH2.f", \
+        "sixs/METH3.f", "sixs/METH4.f", \
+        "sixs/METH5.f", "sixs/METH6.f", \
+        "sixs/MIDSUM.f", "sixs/MIDWIN.f", \
+        "sixs/MIE.f", "sixs/MINNALBE.f", \
+        "sixs/MINNBRDF.f", "sixs/MOCA1.f", \
+        "sixs/MOCA2.f", "sixs/MOCA3.f", \
+        "sixs/MOCA4.f", "sixs/MOCA5.f", \
+        "sixs/MOCA6.f", "sixs/MODISALBE.f", \
+        "sixs/MODISBRDF.f", "sixs/MODIS.f", \
+        "sixs/MSS.f", "sixs/NIOX1.f", \
+        "sixs/NIOX2.f", "sixs/NIOX3.f", \
+        "sixs/NIOX4.f", "sixs/NIOX5.f", \
+        "sixs/NIOX6.f", "sixs/OCEAALBE.f", \
+        "sixs/OCEABRDF.f", "sixs/OCEABRDFFAST.f", \
+        "sixs/OCEA.f", "sixs/OCEATOOLS.f", \
+        "sixs/ODA550.f", "sixs/ODRAYL.f", \
+        "sixs/OS.f", "sixs/OSPOL.f", \
+        "sixs/OXYG3.f", "sixs/OXYG4.f", \
+        "sixs/OXYG5.f", "sixs/OXYG6.f", \
+        "sixs/OZON1.f", "sixs/PLANPOL.f", \
+        "sixs/POLDER.f", "sixs/POLGLIT.f", \
+        "sixs/POLNAD.f", "sixs/POSGE.f", \
+        "sixs/POSGW.f", "sixs/POSLAN.f", \
+        "sixs/POSMTO.f", "sixs/POSNOA.f", \
+        "sixs/POSSOL.f", "sixs/POSSPO.f", 
+        "sixs/PRESPLANE.f", "sixs/PRESSURE.f", \
+        "sixs/PRINT_ERROR.f", "sixs/RAHMALBE.f", \
+        "sixs/RAHMBRDF.f", "sixs/ROUJALBE.f", \
+        "sixs/ROUJBRDF.f", "sixs/SAND.f", \
+        "sixs/SCATRA.f", "sixs/SEAWIFS.f", \
+        "sixs/SOLIRR.f", "sixs/SOOT.f", \
+        "sixs/SPECINTERP.f", "sixs/SPLIE2.f", \
+        "sixs/SPLIN2.f", "sixs/SPLINE.f", \
+        "sixs/SPLINT.f", "sixs/STM.f", \
+        "sixs/SUBSUM.f", "sixs/SUBWIN.f", \
+        "sixs/TM.f", "sixs/TROPIC.f", \
+        "sixs/TRUNCA.f", "sixs/US62.f", \
+        "sixs/VARSOL.f", "sixs/VEGETA.f", \
+        "sixs/VERSALBE.f", "sixs/VERSBRDF.f", \
+        "sixs/VERSTOOLS.f", "sixs/VGT.f", \
+        "sixs/VIIRS.f", "sixs/WALTALBE.f", \
+        "sixs/WALTBRDF.f", "sixs/WATE.f", \
+        "sixs/WAVA1.f", "sixs/WAVA2.f", \
+        "sixs/WAVA3.f","sixs/WAVA4.f", \
+        "sixs/WAVA5.f","sixs/WAVA6.f"])
     return config
 
 if __name__ == "__main__":
-    setup(name='sixs',
-        version=version,
-        description="Python bindings for 6s. The 6S code is a basic RT code used for calculation of lookup tables in the MODIS atmospheric correction algorithm.'",
-        long_description="""\
-    """,
-        classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        keywords='remote_sensing,radiative_transfer,atmospheric_modelling',
-        author='J Gomez-Dans',
-        author_email='j.gomez-dans@ucl.ac.uk',
-        url='http://6s.ltdri.org/',
-        license='',
-        packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-        include_package_data=True,
-        zip_safe=False,
-        install_requires=[
-            # -*- Extra requirements: -*-
-        ],
-        entry_points="""
-        # -*- Entry points: -*-
-        """,
-        )
+    import sys
+    
+    # This is a hack, as F77/F90 flags can be passed in the config object
+    # but only on recent versions of numpy/f2py
+    sys.argv.extend ( ["config_fc", "--fcompiler=gnu95", 
+            "--f90exec=/usr/bin/gfortran44", 
+            "--f77flags='-ffixed-form -ffixed-line-length-none'", 
+            "--f90flags=' -ffixed-form -ffixed-line-length-none'"])
+    name         = "sixs"  # name of the generated python extension (.so)
+    description  = "6s python wrappers"
+    author       = "J Gomez-Dans/NCEO & University College London"
+    author_email = "j.gomez-dans@ucl.ac.uk"
+    url="http://6s.ltdri.com/"
+    
+    setup( name=name,\
+        description=description, \
+        author=author, \
+        author_email = author_email, \
+        url=url,
+        configuration = configuration, version="1.0",\
+        packages=["sixs"])
+    #####setup(name='sixs',
+        #####configuration=configuration,
+        #####version=version,
+        #####description="Python bindings for 6s. The 6S code is a basic RT code used for calculation of lookup tables in the MODIS atmospheric correction algorithm.'",
+        #####long_description="""\
+    #####""",
+        #####classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        #####keywords='remote_sensing,radiative_transfer,atmospheric_modelling',
+        #####author='J Gomez-Dans',
+        #####author_email='j.gomez-dans@ucl.ac.uk',
+        #####url='http://6s.ltdri.org/',
+        #####license='',
+        #####packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+        #####include_package_data=True,
+        #####zip_safe=False,
+        #####install_requires=[
+            ###### -*- Extra requirements: -*-
+        #####],
+        #####entry_points="""
+        ###### -*- Entry points: -*-
+        #####""", 
+        
+        #####)
